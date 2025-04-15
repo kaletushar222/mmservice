6327a4b613b70632884a01fce256e29c2143dc0d
tushar kale <kaletushar222@gmail.com>
2025-04-16 00:55:03 +0530

    Initial setup of spring boot application

---
+++ mmservice/src/main/java/com/mitramandal/mmservice/MmserviceApplication.java
@@ -0,0 +1,13 @@
+package com.mitramandal.mmservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MmserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MmserviceApplication.class, args);
+	}
+
+}
---
+++ mmservice/src/test/java/com/mitramandal/mmservice/MmserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mitramandal.mmservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MmserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
